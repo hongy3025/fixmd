b739c908459d334e6fb3e96f36a470db7d920992
yinghong <yinghong@txcombo.com>
2025-01-03 17:40:14 +0800

    add turndown-plug-gfw

---
+++ turndown-plugin-gfm/strikethrough.js
@@ -0,0 +1,10 @@
+function strikethrough(turndownService) {
+  turndownService.addRule('strikethrough', {
+    filter: ['del', 's', 'strike'],
+    replacement: function (content) {
+      return '~' + content + '~'
+    }
+  })
+}
+
+module.exports = strikethrough;
